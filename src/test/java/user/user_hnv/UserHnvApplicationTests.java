079a4be55e9e9df14a18702b379648be84d01406
HungNV <hung.nv17.06@gmail.com>
2024-10-18 21:37:59 +0900

    first commit

---
+++ src/test/java/user/user_hnv/UserHnvApplicationTests.java
@@ -0,0 +1,13 @@
+package user.user_hnv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
